793bbf8de0b6d67b7c5455e9507b065ae673e7fc
rodgerthat <joel.r.norris@gmail.com>
2018-02-25 22:08:07 -0600

    added timer code and image hiding, loaded assets

--- EmojiMemoryMatch/Form1.cs
+++ EmojiMemoryMatch/Form1.cs
@@ -12,6 +12,15 @@ namespace EmojiMemoryMatch
 {
     public partial class Form1 : Form
     {
+
+        // properties of our Form1 class
+        bool allowClick = false;
+        PictureBox firstGuess;
+        Random rnd = new Random();
+        Timer clickTimer = new Timer();
+        int time = 60;
+        Timer timer = new Timer { Interval = 1000 };    // create a timer that 'ticks' every second
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +29,101 @@ namespace EmojiMemoryMatch
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private PictureBox[] pictureBoxes
+        {
+            get { return Controls.OfType<PictureBox>().ToArray(); }
+        }
+
+        private static IEnumerable<Image> images
+        {
+            get
+            {
+                return new Image[]
+                {
+                    Properties.Resources.img1,
+                    Properties.Resources.img2,
+                    Properties.Resources.img3,
+                    Properties.Resources.img4,
+                    Properties.Resources.img5,
+                    Properties.Resources.img6,
+                    Properties.Resources.img7,
+                    Properties.Resources.img8
+                };
+            }
+        }
+
+        private void startGameTimer()
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time<0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Out of Time");
+                    ResetImages();
+                }
+
+                var ssTime = TimeSpan.FromSeconds(time);
+
+                lable1.Text = "00: " + time.ToString();
+            }
+        }
+
+        private void ResetImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Tag = null;
+                pic.Visible = true;
+
+            }
+
+            HideImages();
+            setRandomImages();
+            time = 60;
+            timer.Start();
+        }
+
+        private void HideImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Image = Properties.Resources.question;
+            }
+        }
+
+        private PictureBox getFreeSlot()
+        {
+            int num;
+
+            do
+            {
+                num = rnd.Next(0, pictureBoxes.Count());
+            }
+            while (pictureBoxes[num].Tag != null);
+
+            return pictureBoxes[num];
+        }
+
+        private void setRandomImages()
+        {
+            foreach (var image in images)
+            {
+                getFreeSlot().Tag = image;
+                getFreeSlot().Tag = image;
+            }
+        }
+
+        private void CLICKTIMER_TICK(object sender, EventArgs e)
+        {
+            HideImages();
+
+            allowClick = true;
+            clickTimer.Stop();
         }
     }
 }
