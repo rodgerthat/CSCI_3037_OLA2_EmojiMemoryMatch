5f22806ad8bef4ab328be1c014ff51b9de16fdf3
rodgerthat <joel.r.norris@gmail.com>
2018-02-26 13:32:40 -0600

    finished base functionality. adjusted form size and pictureboxes to fit cards better

--- EmojiMemoryMatch/Form1.Designer.cs
+++ EmojiMemoryMatch/Form1.Designer.cs
@@ -66,18 +66,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(469, 550);
+            this.button1.Location = new System.Drawing.Point(12, 580);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 25);
+            this.button1.Size = new System.Drawing.Size(94, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.startGame);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(485, 578);
+            this.label1.Location = new System.Drawing.Point(112, 584);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 17);
             this.label1.TabIndex = 1;
@@ -85,153 +86,169 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(27, 28);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.Size = new System.Drawing.Size(94, 136);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(177, 28);
+            this.pictureBox2.Location = new System.Drawing.Point(112, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.Size = new System.Drawing.Size(93, 136);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(321, 28);
+            this.pictureBox3.Location = new System.Drawing.Point(211, 12);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.Size = new System.Drawing.Size(94, 136);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 4;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(469, 28);
+            this.pictureBox4.Location = new System.Drawing.Point(311, 12);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.Size = new System.Drawing.Size(94, 136);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 5;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(469, 159);
+            this.pictureBox5.Location = new System.Drawing.Point(312, 154);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.Size = new System.Drawing.Size(94, 136);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 9;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox6
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(321, 159);
+            this.pictureBox6.Location = new System.Drawing.Point(212, 154);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.Size = new System.Drawing.Size(94, 136);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 8;
             this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox7
             // 
-            this.pictureBox7.Location = new System.Drawing.Point(177, 159);
+            this.pictureBox7.Location = new System.Drawing.Point(112, 154);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.Size = new System.Drawing.Size(94, 136);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox7.TabIndex = 7;
             this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox8
             // 
-            this.pictureBox8.Location = new System.Drawing.Point(27, 159);
+            this.pictureBox8.Location = new System.Drawing.Point(12, 154);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox8.Size = new System.Drawing.Size(94, 136);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox8.TabIndex = 6;
             this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox9
             // 
-            this.pictureBox9.Location = new System.Drawing.Point(469, 288);
+            this.pictureBox9.Location = new System.Drawing.Point(312, 296);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox9.Size = new System.Drawing.Size(94, 136);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox9.TabIndex = 13;
             this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox10
             // 
-            this.pictureBox10.Location = new System.Drawing.Point(321, 288);
+            this.pictureBox10.Location = new System.Drawing.Point(212, 296);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox10.Size = new System.Drawing.Size(94, 136);
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox10.TabIndex = 12;
             this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox11
             // 
-            this.pictureBox11.Location = new System.Drawing.Point(177, 288);
+            this.pictureBox11.Location = new System.Drawing.Point(112, 296);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox11.Size = new System.Drawing.Size(94, 136);
             this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox11.TabIndex = 11;
             this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox12
             // 
-            this.pictureBox12.Location = new System.Drawing.Point(27, 288);
+            this.pictureBox12.Location = new System.Drawing.Point(12, 296);
             this.pictureBox12.Name = "pictureBox12";
-            this.pictureBox12.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox12.Size = new System.Drawing.Size(94, 136);
             this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox12.TabIndex = 10;
             this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox13
             // 
-            this.pictureBox13.Location = new System.Drawing.Point(469, 412);
+            this.pictureBox13.Location = new System.Drawing.Point(312, 438);
             this.pictureBox13.Name = "pictureBox13";
-            this.pictureBox13.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox13.Size = new System.Drawing.Size(94, 136);
             this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox13.TabIndex = 17;
             this.pictureBox13.TabStop = false;
+            this.pictureBox13.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox14
             // 
-            this.pictureBox14.Location = new System.Drawing.Point(321, 412);
+            this.pictureBox14.Location = new System.Drawing.Point(212, 438);
             this.pictureBox14.Name = "pictureBox14";
-            this.pictureBox14.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox14.Size = new System.Drawing.Size(94, 136);
             this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox14.TabIndex = 16;
             this.pictureBox14.TabStop = false;
+            this.pictureBox14.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox15
             // 
-            this.pictureBox15.Location = new System.Drawing.Point(177, 412);
+            this.pictureBox15.Location = new System.Drawing.Point(112, 438);
             this.pictureBox15.Name = "pictureBox15";
-            this.pictureBox15.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox15.Size = new System.Drawing.Size(94, 136);
             this.pictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox15.TabIndex = 15;
             this.pictureBox15.TabStop = false;
+            this.pictureBox15.Click += new System.EventHandler(this.clickImage);
             // 
             // pictureBox16
             // 
-            this.pictureBox16.Location = new System.Drawing.Point(27, 412);
+            this.pictureBox16.Location = new System.Drawing.Point(12, 438);
             this.pictureBox16.Name = "pictureBox16";
-            this.pictureBox16.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox16.Size = new System.Drawing.Size(94, 136);
             this.pictureBox16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox16.TabIndex = 14;
             this.pictureBox16.TabStop = false;
+            this.pictureBox16.Click += new System.EventHandler(this.clickImage);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1046, 616);
+            this.ClientSize = new System.Drawing.Size(419, 614);
             this.Controls.Add(this.pictureBox13);
             this.Controls.Add(this.pictureBox14);
             this.Controls.Add(this.pictureBox15);
--- EmojiMemoryMatch/Form1.cs
+++ EmojiMemoryMatch/Form1.cs
@@ -60,7 +60,7 @@ namespace EmojiMemoryMatch
             timer.Tick += delegate
             {
                 time--;
-                if (time<0)
+                if (time < 0)
                 {
                     timer.Stop();
                     MessageBox.Show("Out of Time");
@@ -69,8 +69,8 @@ namespace EmojiMemoryMatch
 
                 var ssTime = TimeSpan.FromSeconds(time);
 
-                lable1.Text = "00: " + time.ToString();
-            }
+                label1.Text = "00: " + time.ToString();
+            };
         }
 
         private void ResetImages()
@@ -124,6 +124,54 @@ namespace EmojiMemoryMatch
 
             allowClick = true;
             clickTimer.Stop();
+        }
+
+        private void clickImage(object sender, EventArgs e)
+        {
+            if (!allowClick) return;
+
+            var pic = (PictureBox)sender;
+            
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+
+            pic.Image = (Image)pic.Tag;
+
+            if (pic.Image == firstGuess.Image && pic != firstGuess)
+            {
+                pic.Visible = firstGuess.Visible = false;
+                {
+                    firstGuess = pic;
+                }
+                HideImages();
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+
+            firstGuess = null;
+            if (pictureBoxes.Any(p => p.Visible)) return;
+            MessageBox.Show("YOU WIN!", "Now Try Again!");
+            ResetImages();
+
+        }
+
+        private void startGame(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRandomImages();
+            HideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += CLICKTIMER_TICK;
+            button1.Enabled = false;
+
         }
     }
 }
